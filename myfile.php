1fe3c16f92111c9ae1a8207c954adf9d8523c03f
Nancy-omoruyi <86826011+Nancy-omoruyi@users.noreply.github.com>
2021-07-02 15:40:58 +0100

    Add files via upload

---
+++ myfile.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php file</title>
+</head>
+<body>
+
+<form action="myfile.php" method="post">
+Name: <input type="text" name="name">
+<br/>
+Age: <input type="number" name="age">
+<br/>
+Email: <input type="email" email="email">
+<br/> 
+Password: <input type="password" name="password">
+<br/>
+<input type="submit" name="Register">
+
+<?php
+echo $_POST["name"]
+?>
+
+<?php
+echo $_POST["age"]
+?>
+
+<?php
+echo $_POST["email"]
+?>
+
+<?php
+echo $_POST["password"]
+?>
+
+
+
+
+</body>
+</html>
+
+
